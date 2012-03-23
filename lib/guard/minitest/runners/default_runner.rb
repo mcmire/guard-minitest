62a9891d37348fb56f09d9e86e77373849fe6240
Bram Swenson <bram@craniumisajar.com>
2012-03-23 12:26:38 -0700

    added a runner for minitest-reporters SuiteRunner

--- lib/guard/minitest/runners/default_runner.rb
+++ lib/guard/minitest/runners/default_runner.rb
@@ -1,8 +1,13 @@
 # encoding: utf-8
 require 'minitest/unit'
 
-if MiniTest::Unit::VERSION =~ /^1/
+case
+when MiniTest.const_defined?(:SuiteRunner)
+  load File.expand_path('../suite_runner.rb', __FILE__)
+when MiniTest::Unit::VERSION =~ /^1/
   load File.expand_path('../version_1_runner.rb', __FILE__)
-else
+when MiniTest::Unit::VERSION =~ /^2/
   load File.expand_path('../version_2_runner.rb', __FILE__)
+else
+  raise "Unknown MiniTest runner version!"
 end
